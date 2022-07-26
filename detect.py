48e99a5cfc647293bc032de3c59331067b96d790
maxym.huang <maxym.huang@optoma.com>
2022-07-26 15:43:31 +0800

    2nd edit

--- detect.py
+++ detect.py
@@ -7,56 +7,62 @@ class findtype:
     
     def calc_result(self):
         if self.attr == 'Apple TV':
-            return '1'
-        elif self.attr == '日期':
-            return '2'
+            return 1
+        elif self.attr == '安裝數':
+            return 2
         elif self.attr == 'YYYY':
-            return '3'
+            return 3
         else:
-            return '4'
+            return 4
 
 
 
 def getdata():
-    try:
-        result = []
-        file = "D:\Intern\project\data_scraping\demo_files\playstore_demo.csv"
-        with open(file, 'r', encoding ='utf-8') as playdata:
-            data_frame = list(csv.reader(playdata))
-            for row in data_frame:
-                result.append(row)
-        result = result[:4]
-    except:
-        print('Invalid file! Please try again.')
-        return None
+
+    result = []
+    file = "D:\Intern\project\data_scraping\demo_files\Appstore_demo.csv"
+    with open(file, 'r', encoding ='utf-8') as playdata:
+        data_frame = list(csv.reader(playdata))
+        for row in data_frame:
+            result.append(row)
+    result = result[:4]
     # print(result)
     return result
 
 def attr():
     id_list = getdata()
-    # print(id_list)
-    
+    stop = False
+    num = 0
     for id in id_list:
         for list in id:
-            if list.find('日期') != -1:
-                result = findtype('日期')
+            if list.find('安裝數') != -1:
+                result = findtype('安裝數')
                 num = result.calc_result()
+                stop = True
                 break
             elif list.find('Apple TV') != -1:
                 result = findtype('Apple TV')
                 num = result.calc_result()
+                stop = True
                 break
-            elif list.find('YYYYMMDD (UTC)'):
+            elif list.find('YYYYMMDD (UTC)') != -1:
                 result = findtype('YYYYMMDD (UTC)')
                 num = result.calc_result()
-            else:
-                num = '4'
+                stop = True
+                break
+        if stop:
+            break
+
     return num
 
 
 
 print(attr())
+# fucker = getdata()
+# for i in fucker:
+#     for sucker in i:
+#         print(sucker.find('安裝數'))
 
-
+# print(findtype('安裝數').calc_result())
             
 
