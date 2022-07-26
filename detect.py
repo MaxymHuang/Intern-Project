54fc842bcf1b352a15156e1061dd580f2cbd24b7
maxym.huang <maxym.huang@optoma.com>
2022-07-26 15:05:19 +0800

    first edit

--- detect.py
+++ detect.py
@@ -1,13 +1,62 @@
 import csv
 import pandas as pd
 
-def main():
-    with open ("D:\Intern\project\data_scraping\demo_files\appstore_demo.csv", 'r', encoding = 'utf-8') as file:
-        data  = list(csv.reader(file))
-        for d in data:
-            CC = d.find('Creative Cast') 
-            CV = d.find('Collavision')
-            one = d.find('OneLink')
-            
+class findtype:
+    def __init__(self, attr=None):
+        self.attr = attr
+    
+    def calc_result(self):
+        if self.attr == 'Apple TV':
+            return '1'
+        elif self.attr == '日期':
+            return '2'
+        elif self.attr == 'YYYY':
+            return '3'
+        else:
+            return '4'
+
+
+
+def getdata():
+    try:
+        result = []
+        file = "D:\Intern\project\data_scraping\demo_files\playstore_demo.csv"
+        with open(file, 'r', encoding ='utf-8') as playdata:
+            data_frame = list(csv.reader(playdata))
+            for row in data_frame:
+                result.append(row)
+        result = result[:4]
+    except:
+        print('Invalid file! Please try again.')
+        return None
+    # print(result)
+    return result
+
+def attr():
+    id_list = getdata()
+    # print(id_list)
+    
+    for id in id_list:
+        for list in id:
+            if list.find('日期') != -1:
+                result = findtype('日期')
+                num = result.calc_result()
+                break
+            elif list.find('Apple TV') != -1:
+                result = findtype('Apple TV')
+                num = result.calc_result()
+                break
+            elif list.find('YYYYMMDD (UTC)'):
+                result = findtype('YYYYMMDD (UTC)')
+                num = result.calc_result()
+            else:
+                num = '4'
+    return num
+
+
+
+print(attr())
+
+
             
 
