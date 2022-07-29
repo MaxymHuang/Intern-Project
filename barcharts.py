3e1bf644695be748cc2a9c2d1edf48be768ba824
maxym.huang <maxym.huang@optoma.com>
2022-07-29 14:42:48 +0800

    added object oriented function

--- barcharts.py
+++ barcharts.py
@@ -4,31 +4,62 @@ from tkinter import filedialog as fd
 from tkinter.messagebox import showinfo
 import pandas as pd
 
+class Month:
+    def __init__(self, month_list):
+        self.month_list = month_list
+        self.size = len(month_list)
+    
+    def find_days(self, day):
+        if self.size <= 31:
+            return self.size
+        month = self.month_list[day]
+        if (month % 2 == 0 and month < 7 and month != 2) or (month % 2 == 1 and month >= 8):
+            days = 30
+            return days
+        elif month == 2:
+            days = 28
+            return days
+        else:
+            days = 31
+            return days
+    
+    def check_ending(self, index):
+        remaining_days = self.size - index
+        if remaining_days < 30 and self.month_list[index] != 2:
+            return True
+        else:
+            return False
+            
 def findavg(df, days=30):
     new_user = df['New users']
     users = df['Users']
     dates = df['month']
     size = len(users)
-    avg_user = []
-    avg_new = []
-    new_sum = 0
-    user_sum = 0
-    for i in range(1, size, days):
-        if (size - i) < 29 and dates[i] != 2:
-            days = size - (i + 1)
-            # print('yes yes yes')
-        elif dates[i] == 4 or dates[i] == 6 or dates[i] == 9 or dates[i] == 11:
-            days = 30
-            # print('nononono')
-        elif dates[i] == 2:
-            days = 28
-        else:
-            days = 31
-        new_sum = sum(new_user[i:(i + days)])
-        user_sum = sum(users[i:(i + days)])
-        avg_new.append(round(new_sum / days))
-        avg_user.append(round(user_sum / days))
-    return avg_new, avg_user
+    users_sum = 0
+    new_user_sum = 0
+    users_avg = []
+    new_user_avg = []
+    months = Month(dates)
+    days = months.find_days(0)
+    for index in range(0, size, days):
+        day = months.find_days(index)
+        if months.check_ending(index):
+            day = size - index
+        users_sum = sum(users[index:index + day])
+        # print(users_sum)
+        new_user_sum = sum(new_user[index:index + day])
+        users_avg.append(round(users_sum / day))
+        new_user_avg.append(round(new_user_sum / day))
+        # print(users_avg)
+        days = months.find_days(index)
+        # print(day)
+    # print(users_avg)
+    
+    return users_avg, new_user_avg
+
+
+    
+    
 
 def findmonthlist(df):
     month_name = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'July', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
@@ -48,8 +79,9 @@ def findmonthlist(df):
 def main(file):
     df = pd.read_excel(file)
     avg = findavg(df)
-    avg_new = avg[0]
-    avg_user = avg[1] 
+    avg_new = avg[1]
+    # print(avg_new)
+    avg_user = avg[0] 
     months = findmonthlist(df)
     if len(months) > 12:
         avg_new = avg_new[-12:]
@@ -71,3 +103,4 @@ def main(file):
     
 
 
+main("D:\Intern\project\data\CC.xlsx")
